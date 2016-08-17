e683b83f62ea1cf1ee3221cf304ee48a4d69fe8c
kounelios13 <pkounelios@gmail.com>
2016-08-17 19:05:37 +0300

    added removed method in cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,4 +1,5 @@
 import os
+output = open("clearOutput.txt",'w')
 def checkfeed():
 	temp = [f+"\n" for f in os.listdir('.') if os.path.isfile(f)]
 	try:
@@ -21,6 +22,20 @@ def removeEmptyLines(fileName):
 		if len(l.strip())>0:
 			file.write(l)
 	file.close()		
+
 def removeComments(fileName):
-	pass
-shrinkFiles()
+	def isComment(line):
+		comments=["*","//","// ","*/","* ","/*","/**","#"," #"]
+		ending=[" */","*/","**/","#"," #"]
+		for i in comments:
+			if line.startswith(i):
+				return True 
+		for i in ending:
+			if line.endswith(i):
+				return True 	
+		return False				
+	lines = open(fileName,'r').readlines()
+	for line in lines:
+		if not isComment(line.strip()) and len(line.strip())>0:
+			output.write(line)
+removeComments("sample.txt")
