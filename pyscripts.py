52c2e670a51fbddba852b584bdd10d3d21f7d08f
kounelios13 <pkounelios@gmail.com>
2016-10-26 23:13:31 +0300

    remove function

--- pyscripts.py
+++ pyscripts.py
@@ -1,3 +1,4 @@
+import os
 def is_line_empty(line):
 	return len(line.strip()) < 1
 def is_comment(line,isPythonFile = False):
@@ -15,18 +16,8 @@ def is_comment(line,isPythonFile = False):
 	return False
 def get_file_extension(file): 
 	return file.split(".")[1]	
-def file_exists(file):
-	try:
-		f=open(file,'r')
-		f.close()
-	except FileNotFoundError:
-		return False
-	return True	
 def remove_empty_lines(file):
 	lines = []
-	if not file_exists(file):
-		print ("{} does not exist ".format(file))
-		return
 	with open(file,'r') as in_file:
 		lines = in_file.readlines()
 	with open(file,'w') as out:
@@ -38,8 +29,7 @@ def remove_comments(file):
 		pythonExtensions.index(get_file_extension(file))
 	except ValueError:
 		isPython = False
-	f = open(file,'r')
-	lines = f.readlines()
-	f.close()
-	f = open(file,'w')	
-	f.writelines([l for l in lines if not is_comment(l,isPython)])
+	with open(file,'r') as in_file:
+		lines  = in_file.readlines()
+	with open(file,'w') as out:	
+		out.writelines([l for l in lines if not is_comment(l,isPython)])
