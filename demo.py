daedf7239f00f4dbf7befa5694a6cd4983f1dfe9
kounelios13 <pkounelios@gmail.com>
2016-10-29 12:02:01 +0300

    prettified demo code

--- demo.py
+++ demo.py
@@ -1,17 +1,18 @@
 from pyscripts import pyscripts
 def showMenu(file):
-	print("Select 1 to remove empty lines 2 to remove comments 3 to do all of these things:\n")
-	sel = int(input())
+	sel = int(input("Select 1 to remove empty lines 2 to remove comments 3 to do all of these things:\n"))
 	if sel == 3:
 		pyscripts.remove_comments(filename)
 		pyscripts.remove_empty_lines(filename)
+		print("Removed comments and blank lines\n")
 	elif sel == 2:			
 		pyscripts.remove_comments(filename)
+		print("Removed comments\n")
 	elif sel == 1:
-		pyscripts.remove_empty_lines(filename)	
+		pyscripts.remove_empty_lines(filename)
+		print("Removed blank lines\n")	
 	else:
 		return
 filename = input("Give me a file name(with absolute or relative path):")
-#help(pyscripts)
 while True:
 	showMenu(filename)
